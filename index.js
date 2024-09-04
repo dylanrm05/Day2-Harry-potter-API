617bc80c8ed6c9cad7cdc2a0e682097a1782f180
unknown <Dylan Renouf-Martin>
2024-09-04 16:26:44 +0100

    Add cors

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = 4000
 
 const characters = require('./harrypotter.json')
 //middleware
 app.use(express.json())
+app.use(cors())
 
 app.get('/',(req,res)=>{
     res.send("Hello Harry Potter Characters")
